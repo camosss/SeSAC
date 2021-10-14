08dffa03958be5dc5b756802359ac7d9d66418ac
HoSeong <camosss@naver.com>
2021-10-14 15:01:39 +0900

    쇼핑 리스트 파일 생성

---
+++ ssacShoppingList/ssacShoppingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ssacShoppingList
+//
+//  Created by 강호성 on 2021/10/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
