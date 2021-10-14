811f24e3c89a85fa8e095520ac0a73e8af6158ca
HoSeong <camosss@naver.com>
2021-10-14 18:19:39 +0900

    UserDefault

---
+++ ssacShoppingList/ssacShoppingList/Shopping/List.swift
@@ -0,0 +1,12 @@
+//
+//  List.swift
+//  ssacShoppingList
+//
+//  Created by 강호성 on 2021/10/14.
+//
+
+import Foundation
+
+struct List: Codable {
+    var list: String
+}
--- ssacShoppingList/ssacShoppingList/Shopping/ShoppingVC.swift
+++ ssacShoppingList/ssacShoppingList/Shopping/ShoppingVC.swift
@@ -13,28 +13,57 @@ class ShoppingVC: UITableViewController {
     
     @IBOutlet weak var searchTextField: UITextField!
     
-    var shoppingList = ["그립톡 구매하기", "사이다 구매", "아이패드 케이스 최저가 알아보기", "양말"]
+    var shoppingList = [List]()
+    
+    let userDefaults = UserDefaults.standard
     
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        loadData()
+    }
+    
+    // MARK: - Helper
+    
+    func saveData() {
+        let jsonEncoder = JSONEncoder()
+        
+        if let saveData = try? jsonEncoder.encode(shoppingList) {
+            userDefaults.set(saveData, forKey: "List")
+        } else {
+            print("Failed to save data")
+        }
+    }
+    
+    func loadData() {
+        let jsonDecoder = JSONDecoder()
+        
+        if let loadData = userDefaults.object(forKey: "List") as? Data {
+            do {
+                shoppingList = try jsonDecoder.decode([List].self, from: loadData)
+            } catch {
+                print("Failed to load lists")
+            }
+        }
     }
     
     // MARK: - Action
     
     @IBAction func addButton(_ sender: UIButton) {
         if !searchTextField.text!.isEmpty {
-            shoppingList.append(searchTextField.text!)
+            let list = List(list: searchTextField.text!)
+            shoppingList.append(list)
+            saveData()
             tableView.reloadData()
-            
             searchTextField.text = nil
         }
     }
 }
 
 // MARK: - UITableViewDataSource
+
 
 extension ShoppingVC {
     
@@ -44,9 +73,7 @@ extension ShoppingVC {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingCell", for: indexPath) as! ShoppingCell
-        
-        cell.listLabel.text = shoppingList[indexPath.row]
-        cell.listLabel.font = UIFont.systemFont(ofSize: 15)
+        cell.listLabel.text = shoppingList[indexPath.row].list
         return cell
     }
     
@@ -57,8 +84,10 @@ extension ShoppingVC {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             shoppingList.remove(at: indexPath.row)
+            saveData()
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
+    
 }
 
