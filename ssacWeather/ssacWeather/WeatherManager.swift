b5037e01e897766ddac53b039be092e26e18a313
HoSeong <camosss@naver.com>
2021-10-25 21:42:28 +0900

    OpenWeatherMap UI

--- ssacWeather/ssacWeather/WeatherManager.swift
+++ ssacWeather/ssacWeather/WeatherManager.swift
@@ -10,29 +10,15 @@ import CoreLocation
 import Alamofire
 
 struct WeatherManager {
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=3bd336680248f3f4d90c941f6d034673&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=APIkey&units=metric"
     
     func fetchCoordinate(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
         let urlString = "\(weatherURL)&lat=\(latitude)&lon=\(longitude)"
-        requestURL(with: urlString)
+//        getData()
     }
     
-    func requestURL(with urlString: String) {
+    func getData() {
         
-        if let url = URL(string: urlString) {
-            let session = URLSession(configuration: .default)
-            
-            let task = session.dataTask(with: url) { data, response, error in
-                if error != nil { print("Error Request"); return }
-                
-                if let data = data {
-                    let dataString = String(data: data, encoding: .utf8)
-                    print(dataString ?? "")
-                    
-                }
-            }
-            task.resume()
-        }
     }
     
 }
