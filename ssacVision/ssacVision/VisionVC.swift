3f12863059c4aadaed3ea33170c9da711ffbb3b8
HoSeong <camosss@naver.com>
2021-10-28 09:20:18 +0900

    JGProgressHUD

--- ssacVision/ssacVision/VisionVC.swift
+++ ssacVision/ssacVision/VisionVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import JGProgressHUD
 
 class VisionVC: UIViewController {
     
@@ -14,6 +15,8 @@ class VisionVC: UIViewController {
     @IBOutlet weak var postImageView: UIImageView!
     @IBOutlet weak var ageLabel: UILabel!
     @IBOutlet weak var genderLabel: UILabel!
+    
+    let progress = JGProgressHUD()
     
     // MARK: - Lifecycle
     
@@ -25,6 +28,7 @@ class VisionVC: UIViewController {
     // MARK: - Action
     
     @IBAction func requestButton(_ sender: UIButton) {
+        progress.show(in: view)
         VisionAPIManager.shared.fetchFaceData(image: postImageView.image!) { code, json in
             let age = json["result"]["faces"][0]["facial_attributes"]["age"].doubleValue
             self.ageLabel.text = "\(Int(age))세"
@@ -33,7 +37,9 @@ class VisionVC: UIViewController {
             let female = json["result"]["faces"][0]["facial_attributes"]["gender"]["female"]
             
             if male > female { self.genderLabel.text = "남자" } else { self.genderLabel.text = "여자" }
+            self.progress.dismiss()
         }
     }
     
 }
+
