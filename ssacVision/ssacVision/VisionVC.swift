a871d545210acb5c9505ed57f5caef51e9e223c7
HoSeong <camosss@naver.com>
2021-10-28 10:22:47 +0900

    UIImagePickerController

--- ssacVision/ssacVision/VisionVC.swift
+++ ssacVision/ssacVision/VisionVC.swift
@@ -17,12 +17,15 @@ class VisionVC: UIViewController {
     @IBOutlet weak var genderLabel: UILabel!
     
     let progress = JGProgressHUD()
+    let imagePicker = UIImagePickerController()
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        imagePicker.delegate = self
     }
     
     // MARK: - Action
@@ -30,16 +33,37 @@ class VisionVC: UIViewController {
     @IBAction func requestButton(_ sender: UIButton) {
         progress.show(in: view)
         VisionAPIManager.shared.fetchFaceData(image: postImageView.image!) { code, json in
-            let age = json["result"]["faces"][0]["facial_attributes"]["age"].doubleValue
+            let jsonData = json["result"]["faces"][0]["facial_attributes"]
+            let age = jsonData["age"].doubleValue
             self.ageLabel.text = "\(Int(age))세"
             
-            let male = json["result"]["faces"][0]["facial_attributes"]["gender"]["male"]
-            let female = json["result"]["faces"][0]["facial_attributes"]["gender"]["female"]
-            
+            let male = jsonData["gender"]["male"]
+            let female = jsonData["gender"]["female"]
             if male > female { self.genderLabel.text = "남자" } else { self.genderLabel.text = "여자" }
             self.progress.dismiss()
         }
     }
     
+    @IBAction func changeImage(_ sender: UIButton) {
+        self.present(imagePicker, animated: true, completion: nil)
+    }
 }
 
+// MARK: - UIImagePickerControllerDelegate
+
+extension VisionVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // 사진을 촬영하거나, 갤러리에서 사진을 선택한 직후에 실행
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print(#function)
+        
+        if let value = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            postImageView.image = value
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print(#function)
+    }
+}
